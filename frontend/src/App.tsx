1665e48f2d81343b7946f0ff488b01e6580e0d90
westorres9 <wester.torres9@gmail.com>
2022-03-23 15:10:16 -0300

    added image  in home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,10 @@
 import './assets/styles/custom.scss';
 import './App.css';
-import Navbar from 'Components/Navbar';
+import Home from 'pages/Home';
 
 function App() {
   return (
-    <>
-      <Navbar/>
-      <h1>Hello DSCatalog</h1>
-    </>
+    <Home/>
     
   );
 }
