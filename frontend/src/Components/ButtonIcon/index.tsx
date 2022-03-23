493640a83988a1d77ee06926cd0052c6242bb9d8
westorres9 <wester.torres9@gmail.com>
2022-03-23 17:24:43 -0300

    finished breakpoint reponsivity at 922px

---
+++ frontend/src/Components/ButtonIcon/index.tsx
@@ -0,0 +1,11 @@
+import './styles.css';
+
+function ButtonIcon() {
+    return (
+        <button className="btn btn-icon-primary btn-icon">
+            <h6>INICIE AGORA A SUA BUSCA</h6>
+        </button>
+    );
+}
+
+export default ButtonIcon;
--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,4 +1,5 @@
 import { ReactComponent as MainImage } from 'assets/images/main-image.svg';
+import ButtonIcon from 'Components/ButtonIcon';
 import Navbar from "Components/Navbar";
 import './styles.css';
 
@@ -10,6 +11,8 @@ function Home() {
                 <div className="home-card">
                     <div className="home-content-container">
                         <h1>Conheça o melhor catálogo de produtos</h1>
+                        <p>Ajudaremos voce a encontrar os melhores produtos disponíveis no mercado</p>
+                        <ButtonIcon/>
                     </div>
                     <div className="home-image-container">
                         <MainImage/>
