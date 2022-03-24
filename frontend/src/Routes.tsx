be8d7bac204102cd1654a67ac4aa244d20235a26
westorres9 <wester.torres9@gmail.com>
2022-03-23 23:42:42 -0300

    react router dom and routes defined

---
+++ frontend/src/Routes.tsx
@@ -0,0 +1,29 @@
+import Navbar from "Components/Navbar";
+import Admin from "pages/Admin";
+import Catalog from "pages/Catalog";
+import Home from "pages/Home";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+
+
+function Routes() {
+    return (
+        <BrowserRouter>
+            <Navbar/>
+            <Switch>
+                <Route path="/" exact>
+                    <Home/>
+                </Route>
+                <Route path="/products">
+                    <Catalog/>
+                </Route>
+                <Route path="/admin">
+                    <Admin/>
+                </Route>
+            </Switch>
+        
+        </BrowserRouter>
+    );
+}
+
+export default Routes;
--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,30 +1,31 @@
 import { ReactComponent as MainImage } from 'assets/images/main-image.svg';
 import ButtonIcon from 'Components/ButtonIcon';
-import Navbar from "Components/Navbar";
+import { Link } from 'react-router-dom';
 import './styles.css';
 
 function Home() {
-    return (
-        <>
-            <Navbar/>
-            <div className="home-container">
-                <div className="base-card home-card">
-                    <div className="home-content-container">
-                        <div>
-                            <h1>Conheça o melhor catálogo de produtos</h1>
-                            <p>Ajudaremos voce a encontrar os melhores produtos disponíveis no mercado</p>
-                        </div>
-                        
-                        <ButtonIcon/>
-                    </div>
-                    <div className="home-image-container">
-                        <MainImage/>
-                    </div>
-
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <div className="home-container">
+      <div className="base-card home-card">
+        <div className="home-content-container">
+          <div>
+            <h1>Conheça o melhor catálogo de produtos</h1>
+            <p>
+              Ajudaremos voce a encontrar os melhores produtos disponíveis no
+              mercado
+            </p>
+          </div>
+          <Link to="/products">
+            <ButtonIcon />
+          </Link>
+          
+        </div>
+        <div className="home-image-container">
+          <MainImage />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
