a603a22baa5058fb83668e1d92929fd2b5d73d4f
westorres9 <wester.torres9@gmail.com>
2022-03-25 14:16:00 -0300

    product details layout mobile

--- frontend/src/pages/ProductDetails/index.tsx
+++ frontend/src/pages/ProductDetails/index.tsx
@@ -1,7 +1,36 @@
+import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
+import ProductPrice from 'Components/ProductPrice';
+import './styles.css';
+
 function ProductDetails () {
 
     return(
-        <h1>PaginaProductDetails</h1>
+        <div className="product-details-container">
+            <div className="base-card product-details-card">
+                <div className="go-back-container">
+                    <ArrowIcon/>
+                    <h2>VOLTAR</h2>
+                </div>
+                <div className="row">
+                    <div className="col-xl-6">
+                        <div className="img-container">
+                            <img src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg" alt="" />
+                        </div>
+                        <div className="name-price-container">
+                            <h1>Computador i7</h1>
+                            <ProductPrice price={2345.67}/>
+                        </div>
+                    </div>
+                    <div className="col-xl-6">
+                        <div className="description-container">
+                            <h2>Descrição do Produto</h2>
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore, debitis.</p>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
     );
 }
 
