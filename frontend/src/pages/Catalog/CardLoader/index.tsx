37cdc4a586841a4823f660bca200bd61dde0eed9
westorres9 <wester.torres9@gmail.com>
2022-03-31 08:10:56 -0300

    catalog loader

--- frontend/src/pages/Catalog/CardLoader/index.tsx
+++ frontend/src/pages/Catalog/CardLoader/index.tsx
@@ -2,16 +2,19 @@
 import ContentLoader from "react-content-loader";
 
 const CardLoader = () => (
+  <div className="card-loader-container">
     <ContentLoader 
       speed={2}
-      width={400}
+      width={320}
       height={460}
-      viewBox="0 0 400 460"
+      viewBox="0 0 320 460"
       backgroundColor="#f3f3f3"
       foregroundColor="#acabab"
     >
-      <rect x="7" y="18" rx="2" ry="2" width="300" height="" />
+      <rect x="0" y="0" rx="2" ry="2" width="300" height="300" />
     </ContentLoader>
+  </div>
+    
 );
 
 export default CardLoader;
