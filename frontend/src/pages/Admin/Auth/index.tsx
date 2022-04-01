bb36ef59124296da5debeb1b1d1b0faa5b327fdf
westorres9 <wester.torres9@gmail.com>
2022-03-31 21:38:04 -0300

    banner stylizing

--- frontend/src/pages/Admin/Auth/index.tsx
+++ frontend/src/pages/Admin/Auth/index.tsx
@@ -7,7 +7,7 @@ const Auth = () => {
     return(
         <div className="auth-container">
             <div className="auth-banner-container">
-                <h1>Divulgue seus produtos no DSCatalog</h1>
+                <h1>Divulgue seus produtos no DS Catalog</h1>
                 <p>Faça parte do nosso catálogo de divulgação e aumente a venda dos seus produtos</p>
                 <AuthImage/>
             </div>
